6ca522b5c228e7f00fdc9c40fa294f94610277f2
Priyanka004-debug <priyankamathur040904@gmail.com>
2026-02-04 12:18:07 +0530

    Update Valentine_app.py

--- Valentine_app.py
+++ Valentine_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 #1.Page configuration
 st.set_page_config(page_title="For my Wonderful Husband", page_icon="❤️")
 #2.Main title
@@ -9,13 +10,16 @@ st.audio("music.mp3.mpeg")
 st.write("To my dearest husband, on this special day, I want to remind you how much you mean to me. You are my rock, my best friend, and the love of my life.")
 #5.Special message
 st.header("To the Love of My Life")
-st.image("RJ109494.JPG", caption="Our Special Day", use_container_width=True)
+img = Image.open("RJ109494.JPG")
+rotated_image = img.rotate(-90,expand=True)
+st.image(rotated_image,caption="Our Special Day",use_container_width=True)
 #An interactive button
 if st.button("Click Me for a Surprise!"):
     st.write("You are my everything! ❤️❤️❤️")
     
-    st.markdown("###You are my favourite 'True'in the universe of Booleans!")
+    st.markdown("### You are my favourite 'True'in the universe of Booleans!")
 
     st.write("I Love You more than Python loves indentation! 🐍❤️")
+
 
 
