a60eddd26652134af2faee2a99a9c6e759aeb109
Priyanka004-debug <priyankamathur040904@gmail.com>
2026-02-04 12:28:29 +0530

    Update Valentine_app.py

--- Valentine_app.py
+++ Valentine_app.py
@@ -13,23 +13,7 @@ st.header("To the Love of My Life")
 # 1. Open and Rotate (Keep this as is)
 img = Image.open("RJ109494.JPG")
 rotated_image = img.rotate(90, expand=True) 
-
-# 2. Display with smaller size
-# We use 'width=300' to make it smaller.
-st.image(rotated_image, caption="Our Special Day", width=300)
-
-# 3. Add a Border using CSS
-st.markdown(
-    """
-    <style>
-    img {
-        border: 5px solid #FF4B4B;
-        border-radius: 15px;
-    }
-    </style>
-    """,
-    unsafe_allow_value=True
-)
+st.image(rotated_image,caption="Our Special Moment (28-09-2025)",use_container_width=True)
 #An interactive button
 if st.button("Click Me for a Surprise!"):
     st.write("You are my everything! ❤️❤️❤️")
@@ -37,6 +21,7 @@ if st.button("Click Me for a Surprise!"):
     st.markdown("### You are my favourite 'True'in the universe of Booleans!")
 
     st.write("I Love You more than Python loves indentation! 🐍❤️")
+
 
 
 
