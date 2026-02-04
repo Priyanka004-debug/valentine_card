415b41fe41d94ded5cd6a88f4c1e684322a6d186
Priyanka004-debug <priyankamathur040904@gmail.com>
2026-02-04 11:04:27 +0530

    Update Valentine_app.py

--- Valentine_app.py
+++ Valentine_app.py
@@ -9,9 +9,7 @@ st.audio("music.mp3.mpeg")
 st.write("To my dearest husband, on this special day, I want to remind you how much you mean to me. You are my rock, my best friend, and the love of my life.")
 #5.Special message
 st.header("To the Love of My Life")
-st.write("I coded this little website just to say how much I Love You!!!.")
-#6.Image display
-st.image(r"C:\Users\shavi\Downloads\RJ109494.JPG", caption="Our Special Day", use_container_width=True)
+st.image("RJ109494.JPG", caption="Our Special Day", use_container_width=True)
 #An interactive button
 if st.button("Click Me for a Surprise!"):
     st.write("You are my everything! ❤️❤️❤️")
@@ -19,4 +17,5 @@ if st.button("Click Me for a Surprise!"):
     st.markdown("###You are my favourite 'True'in the universe of Booleans!")
 
     st.write("I Love You more than Python loves indentation! 🐍❤️")
+
 
